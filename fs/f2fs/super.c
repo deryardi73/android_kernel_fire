f6e24c5a41ced8ebccf551b346cd9e879033f7c9
deryardi73 <deryardi73@gmail.com>
2026-09-07 16:58:37 +0700

    fs: fix build error

--- fs/f2fs/super.c
+++ fs/f2fs/super.c
@@ -3766,13 +3766,11 @@ try_onemore:
 		err = f2fs_recover_fsync_data(sbi, true);
 		if (err > 0) {
 			if (!f2fs_readonly(sb)) {
-				f2fs_msg(sb, KERN_ERR,
-					"Need to recover fsync data");
+				f2fs_err(sbi, "Need to recover fsync data");
 				err = -EINVAL;
 				goto free_meta;
 			} else {
-				f2fs_msg(sb, KERN_INFO,
-					"drop all fsynced data");
+				f2fs_info(sbi, "drop all fsynced data");
 				err = 0;
 			}
 		}
